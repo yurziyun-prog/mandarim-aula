054c4b9cdbafa6e394cee8631121b0381e54a535
Yuri Yung <yurziyun@gmail.com>
2026-09-05 15:26:36 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
+function entrar(event) {
+    event.preventDefault();
 
+    const mensagem = document.getElementById("loginMessage");
+
+    mensagem.textContent =
+        "Login ainda não conectado. Firebase será configurado na próxima etapa.";
+}
